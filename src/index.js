8d1df32eaeddb8aec11f27323651fa17959cc44d
tlremez <remez1975@gmail.com>
2021-02-08 15:42:01 -0500

    test2

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {createLogger} from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import {searchRobots, requestRobots} from './reducers'
 import 'tachyons';
